a22fc50186a1787290dc3d6f2dc7620a796b5490
baylerblanchard <baylerblanchard@gmail.com>
2024-01-21 14:11:24 -0600

    still need to work on the js

--- js/EaS.js
+++ js/EaS.js
@@ -1,11 +1,14 @@
 function makeGrid(a) {
+    var grid = document.getElementById("grid");
+
     for (var i = 0; i < a; i++) {
         var row = document.createElement("div");
-        row.classList.add("cell");
-        document.getElementById("container").appendChild(row);
+        row.className = "cell";
+        grid.appendChild(row);
+
         for (var j = 0; j < a; j++) {
             var cell = document.createElement("div");
-            cell.classList.add("cell");
+            cell.className = "cell";
             row.appendChild(cell);
         }
     }
