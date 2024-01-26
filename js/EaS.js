25b232a7e4560e2583e317268a128cb70be4f803
baylerblanchard <baylerblanchard@gmail.com>
2024-01-25 23:14:58 -0600

    really like how the grid is working next need to work on the hover function

--- js/EaS.js
+++ js/EaS.js
@@ -4,17 +4,73 @@ var sliderValue;
 function makeGrid(gridSize) {
     var grid = document.getElementById("grid");
 
-    for (var i = 0; i < gridSize; i++) {
-        var row = document.createElement("div");
-        row.className = "row";
-        grid.appendChild(row);
+    if (gridSize == 1) {
 
-        for (var j = 0; j < gridSize; j++) {
-            var cell = document.createElement("div");
-            cell.innerHTML = '<div class="cell-16x16"></div>';
-            row.appendChild(cell);
+        for (var i = 0; i < 5; i++) {
+            var row = document.createElement("div");
+            row.className = "row";
+            grid.appendChild(row);
+
+            for (var j = 0; j < 5; j++) {
+                var cell = document.createElement("div");
+                cell.innerHTML = '<div class="cell-5x5"></div>';
+                row.appendChild(cell);
+            }
         }
-    }
+    }else if(gridSize == 2){
+
+        for (var i = 0; i < 9; i++) {
+            var row = document.createElement("div");
+            row.className = "row";
+            grid.appendChild(row);
+
+            for (var j = 0; j < 9; j++) {
+                var cell = document.createElement("div");
+                cell.innerHTML = '<div class="cell-9x9"></div>';
+                row.appendChild(cell);
+            }
+        }
+    }else if(gridSize == 3 || gridSize == 16){
+
+            for (var i = 0; i < gridSize; i++) {
+                var row = document.createElement("div");
+                row.className = "row";
+                grid.appendChild(row);
+    
+                for (var j = 0; j < gridSize; j++) {
+                    var cell = document.createElement("div");
+                    cell.innerHTML = '<div class="cell-16x16"></div>';
+                    row.appendChild(cell);
+                }
+            }
+        }else if(gridSize == 4){
+
+            for (var i = 0; i < 64; i++) {
+                var row = document.createElement("div");
+                row.className = "row";
+                grid.appendChild(row);
+    
+                for (var j = 0; j < 64; j++) {
+                    var cell = document.createElement("div");
+                    cell.innerHTML = '<div class="cell-64x64"></div>';
+                    row.appendChild(cell);
+                }
+            }
+        }else if(gridSize == 5){
+
+            for (var i = 0; i < 100; i++) {
+                var row = document.createElement("div");
+                row.className = "row";
+                grid.appendChild(row);
+    
+                for (var j = 0; j < 100; j++) {
+                    var cell = document.createElement("div");
+                    cell.innerHTML = '<div class="cell-100x100"></div>';
+                    row.appendChild(cell);
+                }
+            }
+        }
+
 }
 
 function sliderListener() {
@@ -22,21 +78,18 @@ function sliderListener() {
     
     slider.addEventListener("change", function() {
         sliderValue = slider.value;
+        clearSlider();
+
         if(sliderValue == 1){
-            clearGrid();
-            makeGrid(5);
+            makeGrid(1);
         }else if(sliderValue == 2){
-            clearGrid();
-            makeGrid(9);
+            makeGrid(2);
         }else if(sliderValue == 3){
-            clearGrid();
             makeGrid(16);
         }else if(sliderValue == 4){
-            clearGrid();
-            makeGrid(64);
+            makeGrid(4);
         }else if(sliderValue == 5){
-            clearGrid();
-            makeGrid(100);
+            makeGrid(5);
         }
     });
 
@@ -52,5 +105,13 @@ function clearGrid(){
         });
 }
 
+//clear logic for slider
+function clearSlider(){
+    var grid = document.getElementById("grid");
+
+    grid.innerHTML = '';
+}
+
+clearGrid();
 sliderListener();
 makeGrid(gridSize);
